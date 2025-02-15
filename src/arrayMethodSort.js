d75284172b0e36219ead21083fd8786ef91a6716
Alla <allatin444@gmail.com>
2025-02-15 21:09:12 +0200

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -2,11 +2,48 @@
 
 /**
  * Implement method Sort
- */
+ */ /*
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    if (typeof compareFunction !== 'function') {
+      // eslint-disable-next-line no-param-reassign
+      compareFunction = (a, b) => (String(a) > String(b) ? 1 : -1);
+    }
+
+    let swapped;
+
+    do {
+      swapped = false;
+
+      for (let i = 0; i < this.length - 1; i++) {
+        if (compareFunction(this[i], this[i + 1]) > 0) {
+          [this[i], this[i + 1]] = [this[i + 1], this[i]];
+          swapped = true;
+        }
+      }
+    } while (swapped);
+
+    return this;
+  };
+} */
+
+function applyCustomSort() {
+  [].__proto__.sort2 = function (compareFunction) {
+    if (typeof compareFunction !== 'function') {
+      // eslint-disable-next-line no-param-reassign
+      compareFunction = (a, b) => (String(a) > String(b) ? 1 : -1);
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      // eslint-disable-next-line no-shadow
+      for (let i = 0; i < this.length - 1; i++) {
+        if (compareFunction(this[i], this[i + 1]) > 0) {
+          [this[i], this[i + 1]] = [this[i + 1], this[i]];
+        }
+      }
+    }
+
+    return this;
   };
 }
-
 module.exports = applyCustomSort;
